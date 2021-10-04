f623ef2bdd5e789eb6f281d48c7ab861bb2597e3
kobfolson <kobbyfolson@gmail.com>
2021-10-04 16:41:27 +0000

    [ADD] routes set up

--- project/app/db.py
+++ project/app/db.py
@@ -2,6 +2,10 @@
 
 import os
 
+from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+
+
 TORTOISE_ORM = {
     "connections": {"default": os.environ.get("DATABASE_URL")},
     "apps": {
@@ -11,3 +15,13 @@ TORTOISE_ORM = {
         },
     },
 }
+
+
+def init_db(app: FastAPI) -> None:
+    register_tortoise(
+        app,
+        db_url=os.environ.get("DATABASE_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=False,
+        add_exception_handlers=True
+    )
